1135aef68dfc52e0f3aec6fb6195650ef477cc6e
OrageKK <huangkun@huangkundeMacBook-Pro.local>
2016-05-01 00:18:48 +0800

    homework-02

---
+++ 微信电话本-主流框架/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  微信电话本-主流框架
+//
+//  Created by 黄坤 on 16/4/29.
+//  Copyright © 2016年 wzpnyg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 微信电话本-主流框架/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  微信电话本-主流框架
+//
+//  Created by 黄坤 on 16/4/29.
+//  Copyright © 2016年 wzpnyg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
